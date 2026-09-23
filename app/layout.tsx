7073d44ff91e6aff69fdc85c75a57af6cd20f973
arkadacasinos <bezdep.rf@mail.ru>
2026-09-23 21:12:19 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
+        <meta name="yandex-verification" content="3b57c245a285f7a9" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         <meta name="theme-color" content="#1a2326" />
